aded966fb1e0c0e50a99775b2b85275236c52f69
mvishok <stum011852@kvschennairegion.in>
2023-10-24 16:47:01 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,456 @@
+from flask import Flask, jsonify, request
+from flasgger import Swagger
+from core import login, details, timetable, calendar
+import json
+
+from colorama import Fore, Back, Style
+
+app = Flask(__name__)
+app.config['url_sort_key'] = None
+app.config['SWAGGER'] = {
+    'title': 'Vcademia API',
+    'uiversion': 3,
+    'hide_top_bar': True,
+    'specs_route': '/',
+    'css': '/static/css/theme-newspaper.css',
+    "ui_params": {
+        "tagsSorter": "alpha"
+    }
+}
+
+swagger = Swagger(app)
+
+@app.route('/key', methods=['POST'])
+def get_access():
+    """
+    Endpoint to get access key
+    ---
+    tags:
+        - Authentication
+    summary: Get access key for a user
+    description: This endpoint retrieves an access key for a user.
+    consumes:
+        - application/x-www-form-urlencoded
+    parameters:
+        - name: user
+          in: formData
+          type: string
+          required: true
+          description: Username (Net ID including @srmist.edu.in)
+        - name: pass
+          in: formData
+          type: string
+          required: true
+          description: Password
+
+    responses:
+        200:
+            description: Access key retrieved successfully
+            schema:
+                type: object
+                properties:
+                    access_key:
+                        type: string
+        401:
+            description: Unauthorized
+    """
+    try:
+        username = request.form['user']
+        password = request.form['pass']
+    except:
+        return jsonify({'status': 'error', 'message': 'Invalid request'})
+    
+    res = login.getKey(username, password)
+    return jsonify(res)
+
+@app.route('/course', methods=['POST'])
+def get_course():
+    """
+    Endpoint to get course details
+    ---
+    tags:
+        - Portal Information
+    summary: Get course details using an access key
+    description: This endpoint retrieves course details based on the provided access key.
+    consumes:
+        - application/x-www-form-urlencoded
+    parameters:
+        - name: key
+          in: formData
+          type: string
+          required: true
+          description: Access key obtained from the 'get_access' endpoint.
+
+    responses:
+        200:
+            description: Course details retrieved successfully
+            schema:
+                type: object
+                properties:
+                    course:
+                        type: object
+                        properties:
+                            "Course Code":
+                                type: array
+                                items:
+                                    type: array
+                            "status":
+                                type: string
+                        example:
+                            "Course Code": [
+                                "Course Title",
+                                "Credit",
+                                "Registration Type",
+                                "Category",
+                                "Course Type",
+                                "Faculty Name",
+                                "Slot",
+                                "GCR Code",
+                                "Room Number",
+                                "Academic Year"
+                            ]
+                            "status": "success"
+    """
+    try:
+        key = request.form['key']
+    except:
+        return jsonify({'status': 'error', 'message': 'Invalid request'})
+    
+    cookie = login.fetchCookies(key)
+    if cookie['status'] == 'error':
+        return jsonify(cookie)
+    cookie = json.loads(cookie['cookie'].replace("'", "\""))
+    
+    try:
+        res = details.course(cookie)
+    except:
+        return jsonify({'status': 'error', 'message': 'Error processing request, please try again later'})
+    return jsonify(res)
+
+@app.route('/details', methods=['POST'])
+def get_details():
+    """
+    Endpoint to get student details
+    ---
+    tags:
+        - Portal Information
+    summary: Get student details using an access key
+    description: This endpoint retrieves student details for a user using an access key.
+    consumes:
+        - application/x-www-form-urlencoded
+    parameters:
+        - name: key
+          in: formData
+          type: string
+          required: true
+          description: Access key obtained from the authentication endpoint.
+
+    responses:
+        200:
+            description: Student details retrieved successfully
+            schema:
+                type: object
+                properties:
+                    status:
+                        type: string
+                        example: success
+                    details:
+                        type: object
+                        properties:
+                            "Class Room:":
+                                type: integer
+                                example: "000"
+                            "Combo / Batch:":
+                                type: string
+                                example: "0/0"
+                            Department:
+                                type: string
+                                example: "Department Name (A1 - Section)"
+                            Mobile:
+                                type: integer
+                                example: "0000000000"
+                            Name:
+                                type: string
+                                example: "Student Name"
+                            Program:
+                                type: string
+                                example: "Enrolled Programme"
+                            "Registration Number:":
+                                type: string
+                                example: "RA0000000000000"
+                            Semester:
+                                type: integer
+                                example: "0"
+        401:
+            description: Unauthorized
+        500:
+            description: Internal Server Error
+    """
+    
+    try:
+        key = request.form['key']
+    except:
+        return jsonify({'status': 'error', 'message': 'Invalid request'})
+    
+    cookie = login.fetchCookies(key)
+    if cookie['status'] == 'error':
+        return jsonify(cookie)
+    cookie = json.loads(cookie['cookie'].replace("'", "\""))
+    
+    try:
+        res = details.student(cookie)
+    except:
+        return jsonify({'status': 'error', 'message': 'Error processing request, please try again later'})
+    
+    print(Fore.GREEN + "[SUCCESS]" + Style.RESET_ALL + " All routes passed successfully with status code 200 OK")
+    return jsonify(res)
+
+@app.route('/attendance', methods=['POST'])
+def get_attendance():
+    """
+    Endpoint to get attendance details
+    ---
+    tags:
+        - Portal Information
+    summary: Get attendance details using an access key
+    description: This endpoint retrieves attendance details for a user using an access key.
+    consumes:
+        - application/x-www-form-urlencoded
+    parameters:
+        - name: key
+          in: formData
+          type: string
+          required: true
+          description: Access key obtained from the authentication endpoint.
+
+    responses:
+        200:
+            description: Attendance details retrieved successfully
+            schema:
+                type: object
+                properties:
+                    status:
+                        type: string
+                        example: success
+                    attendance:
+                        type: object
+                        properties:
+                            Course Title:
+                                type: array
+                                items:
+                                    type: object
+                                    properties:
+                                        Category:
+                                            type: string
+                                            example: "Category"
+                                        "Faculty Name":
+                                            type: string
+                                            example: "Faculty Name"
+                                        Slot:
+                                            type: string
+                                            example: "Slot"
+                                        "Hours Conducted":
+                                            type: string
+                                            example: "Hours Conducted"
+                                        "Hours Absent":
+                                            type: string
+                                            example: "Hours Absent"
+                                        Attendance:
+                                            type: string
+                                            example: "Attendance"
+        401:
+            description: Unauthorized
+        500:
+            description: Internal Server Error
+    """
+    try:
+        key = request.form['key']
+    except:
+        return jsonify({'status': 'error', 'message': 'Invalid request'})
+    
+    cookie = login.fetchCookies(key)
+    if cookie['status'] == 'error':
+        return jsonify(cookie)
+    cookie = json.loads(cookie['cookie'].replace("'", "\""))
+    
+    try:
+        res = details.attendance(cookie)
+    except:
+        return jsonify({'status': 'error', 'message': 'Error processing request, please try again later'})
+    
+    return jsonify(res)
+
+@app.route('/marks', methods=['POST'])
+def get_marks():
+    """
+    Endpoint to get marks details
+    ---
+    tags:
+        - Portal Information
+    summary: Get marks details using an access key
+    description: This endpoint retrieves marks details for a user using an access key.
+    consumes:
+        - application/x-www-form-urlencoded
+    parameters:
+        - name: key
+          in: formData
+          type: string
+          required: true
+          description: Access key obtained from the authentication endpoint.
+
+    responses:
+        200:
+            description: Marks details retrieved successfully
+            schema:
+                type: object
+                properties:
+                    status:
+                        type: string
+                        example: success
+                    marks:
+                        type: object
+                        properties:
+                            Course Code:
+                                type: array
+                                items:
+                                    type: string
+                                    example: "Course Type"
+                            "Test Performance":
+                                type: array
+                                items:
+                                    type: string
+                                    example: "Test Performance"
+        401:
+            description: Unauthorized
+        500:
+            description: Internal Server Error
+    """
+    try:
+        key = request.form['key']
+    except:
+        return jsonify({'status': 'error', 'message': 'Invalid request'})
+    
+    cookie = login.fetchCookies(key)
+    if cookie['status'] == 'error':
+        return jsonify(cookie)
+    cookie = json.loads(cookie['cookie'].replace("'", "\""))
+    
+    try:
+        res = details.marks(cookie)
+    except:
+        return jsonify({'status': 'error', 'message': 'Error processing request, please try again later'})
+    return jsonify(res)
+
+@app.route('/timetable', methods=['POST'])
+def get_timetable():
+    """
+    Endpoint to get timetable details
+    ---
+    tags:
+        - Portal Information
+    summary: Get timetable details using an access key
+    description: This endpoint retrieves timetable details based on the provided access key.
+    consumes:
+        - application/x-www-form-urlencoded
+    parameters:
+        - name: key
+          in: formData
+          type: string
+          required: true
+          description: Access key obtained from the 'get_access' endpoint.
+
+    responses:
+        200:
+            description: Timetable details retrieved successfully
+            schema:
+                type: object
+                properties:
+                    timetable:
+                        type: object
+                        properties:
+                            "Hour/Day Order":
+                                type: array
+                                items:
+                                    type: string
+                            status:
+                                type: string
+                        example:
+                            "Hour/Day Order": [
+                                "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"
+                            ]
+                            "status": "success"
+    """
+    try:
+        key = request.form['key']
+    except:
+        return jsonify({'status': 'error', 'message': 'Invalid request'})
+    
+    cookie = login.fetchCookies(key)
+    if cookie['status'] == 'error':
+        return jsonify(cookie)
+    cookie = json.loads(cookie['cookie'].replace("'", "\""))
+    
+    try:
+        res = timetable.fetch(cookie)
+    except:
+        return jsonify({'status': 'error', 'message': 'Error processing request, please try again later'})
+    
+    return jsonify(res)
+
+@app.route('/calendar', methods=['POST'])
+def get_calendar():
+    """
+    Endpoint to get calendar details
+    ---
+    tags:
+        - Portal Information
+    summary: Get calendar details using an access key
+    description: This endpoint retrieves calendar details based on the provided access key.
+    consumes:
+        - application/x-www-form-urlencoded
+    parameters:
+        - name: key
+          in: formData
+          type: string
+          required: true
+          description: Access key obtained from the 'get_access' endpoint.
+
+    responses:
+        200:
+            description: Calendar details retrieved successfully
+            schema:
+                type: object
+                properties:
+                    calendar:
+                        type: object
+                        properties:
+                            "MMM 'YY":
+                                type: array
+                                items:
+                                    type: array
+                                    items:
+                                        type: string
+                            status:
+                                type: string
+                        example:
+                            "MMM 'YY": [
+                                ["Date", "Day", "Event", "DO", "-"]
+                            ]
+                            "status": "success"
+    """
+    try:
+        key = request.form['key']
+    except:
+        return jsonify({'status': 'error', 'message': 'Invalid request'})
+    
+    cookie = login.fetchCookies(key)
+    if cookie['status'] == 'error':
+        return jsonify(cookie)
+    cookie = json.loads(cookie['cookie'].replace("'", "\""))
+
+    res = calendar.fetch(cookie)
+    
+    return jsonify(res)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ core/login.py
@@ -0,0 +1,110 @@
+import requests
+import json
+from secrets import token_hex
+
+from sys import path as syspath
+from os import path as ospath
+syspath.append(ospath.abspath(ospath.join(ospath.dirname(__file__), '..')))
+from module import crypto, db, constants
+
+URL = "https://academia.srmist.edu.in/accounts/signin.ac"
+
+
+def getCookies(user, passw):
+    """
+    The getCookies function sends a POST request with user credentials to a specified URL, retrieves
+    cookies from the response, and returns them as a token.
+    
+    :param user: The `user` parameter is the username or email address of the user trying to log in
+    :param passw: The `passw` parameter is the password for the user's account
+    :return: a dictionary with the following keys:
+    - "status": indicating the status of the request ("success" or "error")
+    - "token": containing the cookies obtained from the request, if the request was successful
+    - "error": containing the error message, if there was an error in the request
+    """
+    payload = {
+        'username': user,
+        'password': passw,
+        'client_portal': 'true',
+        'portal': '10002227248',
+        'servicename': 'ZohoCreator',
+        'serviceurl': 'https://academia.srmist.edu.in/',
+        'grant_type': 'password',
+        'service_language': 'en',
+        'is_ajax': 'true',
+        'dcc': 'true'
+    }
+
+    try:
+        res = requests.post(URL, data=payload, headers=constants.HEADERS)
+        res = json.loads(res.text)
+        
+        if 'error' in res.keys():
+            return {"status": "error", "error": res['error']}
+        
+        session = requests.Session()
+        session.get(res['data']['oauthorize_uri'], headers=constants.HEADERS)
+        cookies = session.cookies.get_dict()
+        res = {"status": "success", "token": cookies}
+
+    except Exception as e:
+        res = {"status": "error", "error": str(e)}
+
+    return res
+
+def saveCookies(cookies):
+    """
+    The function saves encrypted cookies in a database table and returns a unique key for the saved
+    cookies.
+    
+    :param cookies: The "cookies" parameter is a string that represents the cookies data that needs to
+    be saved
+    :return: either the generated key or the string "DBERROR" depending on the outcome of the try block.
+    """
+    try:
+        key = token_hex(8)
+        cookies = crypto.encrypt(cookies).decode('utf-8')
+        sql = """INSERT INTO sessions (`key`, `session`) VALUES (%s, %s)"""
+        db.cursor.execute(sql, (key, cookies))
+        db.db.commit()
+        return key
+    except Exception as e:
+        return "DBERROR"
+
+def fetchCookies(key):
+    """
+    The function fetchCookies retrieves a session cookie from a database using a given key, decrypts it,
+    and returns it along with a success status, or returns an error status if the key is invalid.
+    
+    :param key: The `key` parameter is a unique identifier used to retrieve a session cookie from the
+    database
+    :return: The function fetchCookies returns a dictionary with two keys: "status" and either "cookie"
+    or "error". If the key is valid and a session is found in the database, the status is set to
+    "success" and the decrypted session cookie is returned as the value for the "cookie" key. If the key
+    is invalid or no session is found, the status is set to "error
+    """
+    sql = """SELECT session FROM sessions WHERE `key` = %s"""
+    db.cursor.execute(sql, (key,))
+    res = db.cursor.fetchone()
+    if res:
+        return {"status": "success", "cookie": crypto.decrypt(res[0].encode('utf-8'))}
+    else:
+        return {"status": "error", "error": "Invalid key"}
+
+def getKey(user, passw):
+    """
+    The function `getKey` takes a username and password as input, retrieves cookies using the
+    `getCookies` function, and returns a dictionary containing the status and key if successful, or an
+    error message if unsuccessful.
+    
+    :param user: The "user" parameter is the username or user ID of the user trying to log in
+    :param passw: The parameter "passw" is likely the password for the user
+    :return: a dictionary with two keys: "status" and either "key" or "error". If the status is
+    "success", it will also include the key value.
+    """
+    cookies = getCookies(user, passw)
+    if cookies['status'] == 'success':
+        key = saveCookies(str(cookies['token']))
+        return {"status": "success", "key": key}
+    else:
+        return {"status": "error", "error": cookies['error']}
